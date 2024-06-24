b1839a1ec498cd388b1faf5791e1ee3d9875ff18
Alvaro Maia <alvaromaia@ciandt.com>
2024-06-24 09:40:19 -0300

    changing basepath

--- src/tst.ts
+++ src/tst.ts
@@ -2,6 +2,14 @@ import fs, { PathLike } from 'fs'
 
 export function tst(filepath: PathLike): boolean {
   console.log(filepath)
+  const testFolder = './';
+
+  fs.readdir(testFolder, (err, files) => {
+    files.forEach(file => {
+      console.log(file);
+    });
+  });
+  
   if (fs.existsSync(filepath)) {
     console.log('exists')
     return true
